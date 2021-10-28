105403d57152f9531020090aba2ad65dbf7230b6
rannye <rannye.rodrigues@justdigital.com.br>
2021-10-28 20:16:51 -0300

    removing app/code initial

--- app/code/Rarod/Sellers/view/adminhtml/requirejs-config.js
+++
@@ -1,8 +0,0 @@
-var config = {
-    "map": {
-        "*": {
-            "Magento_InventoryCatalogAdminUi/js/product/grid/cell/quantity-per-source": "Rarod_Sellers/js/product/grid/cell/quantity-per-source",
-            "Magento_InventorySalesAdminUi/js/product/grid/cell/salable-quantity": "Rarod_Sellers/js/product/grid/cell/salable-quantity",
-        }
-    }
-}
